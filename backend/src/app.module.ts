4e8b392c1fd140578819c10e11173c12e14334dc
Praneeth Kumar <124180295+PraneethKumar189@users.noreply.github.com>
2024-12-16 14:29:28 +0530

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CoursesModule } from './courses/courses.module';
-import { Database } from './database/database';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './user/schema/user.schema';
+
 
 @Module({
-  imports: [UserModule, CoursesModule],
+  imports: [UserModule, CoursesModule,MongooseModule.forRoot('mongodb://127.0.0.1.27017',{dbName:'SkillTrove'}),MongooseModule.forFeature([{
+    name: 'User',
+    schema:UserSchema
+  }])],
   controllers: [AppController],
-  providers: [AppService, Database],
+  providers: [AppService],
 })
 export class AppModule {}
