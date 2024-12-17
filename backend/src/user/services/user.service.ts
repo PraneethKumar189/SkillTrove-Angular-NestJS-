208360263081d5eeb8cfcfdabc08f84d7af7bd63
Praneeth Kumar <124180295+PraneethKumar189@users.noreply.github.com>
2024-12-17 12:19:29 +0530

    Update user.service.ts

--- backend/src/user/services/user.service.ts
+++ backend/src/user/services/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User } from "../schema/user.schema";
@@ -12,7 +12,20 @@ export class Userservice{
 
 
     async createUser(createUserDTO:CreateUserDTO):Promise<IUser>{
-        
-
+        const newUser= await new this.userModel(createUserDTO)
+        return newUser.save()
+          
     }
+
+    async getAllUser():Promise<IUser[]>{
+        const ifUserExists= await  this.userModel.find()
+        if(!ifUserExists)
+        {
+            throw new NotFoundException("User not found")
+        }
+
+        return ifUserExists
+    }
+
+
 }
