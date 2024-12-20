f6e4517a23482473fd9def81276942bff0f94f94
Praneeth Kumar <124180295+PraneethKumar189@users.noreply.github.com>
2024-12-20 11:59:34 +0530

    Update user.controller.ts

--- backend/src/user/user/user.controller.ts
+++ backend/src/user/user/user.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res,  } from '@nestjs/common';
+
+import { CreateUserDTO } from '../dto/createUser.dto';
+import { Userservice } from '../services/user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private dss:Userservice){}
+
+  @Post()
+  async createUser(@Res() response,@Body() CUD:CreateUserDTO ){
+    try{
+        const newUser= await this.dss.createUser(CUD);
+       
+        return response.status(HttpStatus.CREATED).json(
+            {
+                message:'New user created',
+                newUser
+            
+            }
+        )
+    }
+    catch(err)
+    {
+        return response.status(HttpStatus.BAD_REQUEST).json({
+            message:'new user not created',
+            err:'bad request'
+        })
+    }
+   
+    
+    
+}
+}
